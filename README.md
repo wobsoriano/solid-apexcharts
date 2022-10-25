625102e38331c731c4c90b5c9786f544686a9289
Robert Soriano <sorianorobertc@gmail.com>
2022-10-25 14:45:29 -0700

    readme update

--- README.md
+++ README.md
@@ -26,16 +26,18 @@ const App = () => {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
     },
   });
-  const [series] = createStore([
-    {
-      name: 'series-1',
-      data: [30, 40, 35, 50, 49, 60, 70, 91],
-    },
-  ]);
+  const [series] = createStore({
+    list: [
+      {
+        name: 'series-1',
+        data: [30, 40, 35, 50, 49, 60, 70, 91],
+      },
+    ]
+  });
 
   // options and series can be a store or signal
 
-  return <SolidApexCharts width="500" type="bar" options={options} series={series} />;
+  return <SolidApexCharts width="500" type="bar" options={options} series={series.list} />;
 };
 
 export default App;
--- playground/vite/src/App.tsx
+++ playground/vite/src/App.tsx
@@ -1,5 +1,6 @@
 import { createStore } from 'solid-js/store';
 import { SolidApexCharts } from 'solid-apexcharts'
+import { onMount } from 'solid-js';
 
 const App = () => {
   const [options] = createStore({
@@ -10,13 +11,22 @@ const App = () => {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
     },
   });
-  const [series] = createStore({
+  const [series, setSeries] = createStore({
     list: [
       {
         name: 'series-1',
         data: [30, 40, 35, 50, 49, 60, 70, 91],
       },
     ],
+  });
+
+  onMount(() => {
+    setTimeout(() => {
+      setSeries('list', prev => [...prev, {
+        name: 'series-2',
+        data: [40, 21, 35, 50, 49, 60, 70, 100],
+      }]);
+    }, 1000);
   });
 
   return <SolidApexCharts width="500" type="bar" options={options} series={series.list} />;
