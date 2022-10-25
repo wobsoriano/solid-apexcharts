bb12f858679575240bcf7428321a6a0386212088
Robert Soriano <sorianorobertc@gmail.com>
2022-10-25 10:37:19 -0700

    update to latest solid lib starter

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,5 @@ import withSolid from 'rollup-preset-solid';
 
 export default withSolid({
   input: 'src/index.tsx',
-  printInstructions: true,
-  external: ['apexcharts/dist/apexcharts.esm.js'],
+  targets: ['esm', 'cjs'],
 });
