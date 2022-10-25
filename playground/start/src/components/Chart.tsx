d97aad7db7f1b35859ebb40dd924260c0fd62340
Robert Soriano <sorianorobertc@gmail.com>
2022-10-25 13:15:26 -0700

    update ssr example

---
+++ playground/start/src/components/Chart.tsx
@@ -0,0 +1,23 @@
+import { createStore } from 'solid-js/store';
+import { SolidApexCharts } from 'solid-apexcharts';
+
+const Chart = () => {
+  const [options] = createStore({
+    chart: {
+      id: 'solidchart-example',
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+  });
+  const [series] = createStore([
+    {
+      name: 'series-1',
+      data: [30, 40, 35, 50, 49, 60, 70, 91],
+    },
+  ]);
+
+  return <SolidApexCharts width="500" type="bar" options={options} series={series} />;
+};
+
+export default Chart;
--- playground/start/src/routes/index.tsx
+++ playground/start/src/routes/index.tsx
@@ -1,35 +1,11 @@
 import { Title, unstable_clientOnly } from 'solid-start';
-import { createStore } from 'solid-js/store';
-// import { SolidApexCharts } from 'solid-apexcharts';
-// @ts-expect-error: idk
-const { SolidApexCharts } = unstable_clientOnly(() => import('solid-apexcharts'));
-
-const Chart = () => {
-  const [options] = createStore({
-    chart: {
-      id: 'solidchart-example',
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-    },
-  });
-  const [series] = createStore({
-    list: [
-      {
-        name: 'series-1',
-        data: [30, 40, 35, 50, 49, 60, 70, 91],
-      },
-    ],
-  });
-
-  return <SolidApexCharts fallback={<div>hello</div>} width="500" type="bar" options={options} series={series.list} />;
-};
+const SolidApexCharts = unstable_clientOnly(() => import('~/components/Chart'));
 
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <Chart />
+      <SolidApexCharts fallback={<div>hello</div>} />
     </main>
   );
 }
