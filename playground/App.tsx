fc5b701983312de7b1ff66ced8914a7f64de30f3
Robert Soriano <sorianorobertc@gmail.com>
2022-10-25 11:50:59 -0700

    update playground

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,5 +1,4 @@
 import { createStore } from 'solid-js/store';
-// import { createSignal } from 'solid-js';
 
 import { SolidApexCharts } from 'solid-apexcharts';
 
@@ -12,20 +11,18 @@ const App = () => {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
     },
   });
-  const [series] = createStore({
-    list: [
-      {
-        name: 'series-1',
-        data: [30, 40, 35, 50, 49, 60, 70, 91],
-      },
-    ],
-  });
+  const [series] = createStore([
+    {
+      name: 'series-1',
+      data: [30, 40, 35, 50, 49, 60, 70, 91],
+    },
+  ]);
 
   // options and series can be a store or signal
 
   return (
     <div>
-      <SolidApexCharts width="500" type="bar" options={options} series={series.list} />
+      <SolidApexCharts width="500" type="bar" options={options} series={series} />
     </div>
   );
 };
