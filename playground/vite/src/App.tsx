908110dcf9e6fc96a955365396ace0f8368ec19e
wobsoriano <sorianorobertc@gmail.com>
2023-06-05 14:45:49 -0700

    update vite playground

--- playground/vite/src/App.tsx
+++ playground/vite/src/App.tsx
@@ -1,11 +1,10 @@
-import { createStore } from 'solid-js/store';
 import { SolidApexCharts } from 'solid-apexcharts';
-import { onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import type ApexCharts from 'apexcharts';
 
 function App() {
   let chartRef: ApexCharts;
-  const [options] = createStore({
+  const [options] = createSignal({
     chart: {
       id: 'solidchart-example',
     },
@@ -13,7 +12,7 @@ function App() {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
     },
   });
-  const [series] = createStore({
+  const [series] = createSignal({
     list: [
       {
         name: 'series-1',
@@ -24,11 +23,11 @@ function App() {
 
   onMount(() => {
     setTimeout(() => {
-      chartRef?.destroy();
+      // chartRef?.destroy();
     }, 2000);
   });
 
-  return <SolidApexCharts ref={chartRef!} width="500" type="bar" options={options} series={series.list} />;
+  return <SolidApexCharts ref={chartRef!} width="500" type="bar" options={options()} series={series().list} />;
 }
 
 export default App;
