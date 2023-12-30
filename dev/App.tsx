3822aa5012a814900782e5f3bbd627bc9857846c
wobsoriano <sorianorobertc@gmail.com>
2023-12-30 13:36:49 -0800

    simplify dev playground

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,13 +1,10 @@
-import { type ApexChartProps, SolidApexCharts } from 'solid-apexcharts';
 import { createSignal, onMount } from 'solid-js';
 import type ApexCharts from 'apexcharts';
+import { type ApexChartProps, SolidApexCharts } from 'src';
 
 function App() {
   let chartRef: ApexCharts;
   const [options] = createSignal<ApexChartProps['options']>({
-    chart: {
-      id: 'solidchart-example',
-    },
     xaxis: {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
     },
