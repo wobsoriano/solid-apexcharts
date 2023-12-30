3e19e99b686a1ead44d232728b7828d64d04ce95
wobsoriano <sorianorobertc@gmail.com>
2023-12-30 14:48:43 -0800

    update playground

--- dev/App.tsx
+++ dev/App.tsx
@@ -4,6 +4,7 @@ import { type ApexChartProps, SolidApexCharts } from 'src'
 
 function App() {
   let chartRef: ApexCharts
+  const [type, setType] = createSignal<ApexChartProps['type']>('bar')
   const [options] = createSignal<ApexChartProps['options']>({
     xaxis: {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
@@ -25,13 +26,16 @@ function App() {
   })
 
   return (
-    <SolidApexCharts
-      ref={chartRef!}
-      width="500"
-      type="bar"
-      options={options()}
-      series={series().list}
-    />
+    <div>
+      <SolidApexCharts
+        ref={chartRef!}
+        width="500"
+        type={type()}
+        options={options()}
+        series={series().list}
+      />
+      <button onClick={() => setType((prev) => prev === 'line' ? 'bar' : 'line')}>Change type</button>
+    </div>
   )
 }
 
