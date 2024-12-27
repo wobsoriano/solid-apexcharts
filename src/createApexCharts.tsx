891332fe88e9afe7a6c9d08905545ac99bd40de9
wobsoriano <sorianorobertc@gmail.com>
2024-12-27 10:45:07 -0800

    chore: remove deprecated createApexCharts

--- src/createApexCharts.tsx
+++
@@ -1,8 +0,0 @@
-import ApexCharts from 'apexcharts/dist/apexcharts.esm.js'
-
-/**
- * @deprecated Import `ApexCharts` directly from `apexcharts` module instead.
- */
-export function createApexCharts() {
-  return ApexCharts
-}
